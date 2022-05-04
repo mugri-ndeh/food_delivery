e0d667061a0455fb0f30a3a6bbf238fcafbe0328
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-04 14:37:50 +0100

    added const lol

--- lib/auth/screens/signup.dart
+++ lib/auth/screens/signup.dart
@@ -7,7 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
-  SignUp({Key? key}) : super(key: key);
+  const SignUp({Key? key}) : super(key: key);
 
   @override
   State<SignUp> createState() => _SignUpState();
