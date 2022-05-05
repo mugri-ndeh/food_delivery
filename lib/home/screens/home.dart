d984293ccd2ec0fd5e85329fc894f8bfe68c49ba
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-05 18:53:39 +0100

    close to done

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:delivery_app/auth/widgets/custom_fields.dart';
+import 'package:delivery_app/home/models/food_item.dart';
 import 'package:delivery_app/util/palette.dart';
 import 'package:flutter/material.dart';
 
@@ -33,6 +34,51 @@ class _HomeState extends State<Home> {
   ];
   int _selectedindex = 0;
 
+  List<FoodItem> fooditems = [
+    FoodItem(
+      id: '1',
+      price: 1000,
+      name: 'Eru',
+      imageSrc: 'assets/images/eru1.jpg',
+      category: 'category',
+    ),
+    FoodItem(
+      id: '2',
+      price: 1000,
+      name: 'Achu',
+      imageSrc: 'assets/images/achu.jpg',
+      category: 'category',
+    ),
+    FoodItem(
+      id: '3',
+      price: 1000,
+      name: 'Ekpang',
+      imageSrc: 'assets/images/expang.jpg',
+      category: 'category',
+    ),
+    FoodItem(
+      id: '4',
+      price: 1000,
+      name: 'Puff puff and Beans',
+      imageSrc: 'assets/images/puffbeans.jpg',
+      category: 'category',
+    ),
+    FoodItem(
+      id: '5',
+      price: 1000,
+      name: 'Deje',
+      imageSrc: 'assets/images/deje.jpeg',
+      category: 'category',
+    ),
+    FoodItem(
+      id: '6',
+      price: 1000,
+      name: 'Rice and Stew',
+      imageSrc: 'assets/images/ricestew.jpeg',
+      category: 'category',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -54,174 +100,200 @@ class _HomeState extends State<Home> {
           SizedBox(width: 5)
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 150,
-                child: Stack(children: [
-                  Container(
-                    height: 130,
-                    decoration: BoxDecoration(
-                      color: Palette.scaffoldBg,
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20)),
-                      boxShadow: [
-                        BoxShadow(
-                            offset: const Offset(0, 1),
-                            color: Palette.primaryGreen.withOpacity(0.09),
-                            blurRadius: 2),
-                      ],
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Find Your',
-                              style: TextStyle(
-                                  fontSize: 28, fontWeight: FontWeight.bold),
-                            ),
-                            Text('Favourite Food',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                )),
-                          ],
-                        ),
-                        CircleAvatar(
-                          radius: 26,
-                          child: CircleAvatar(
-                              radius: 24,
-                              foregroundColor: Colors.transparent,
-                              backgroundColor: Palette.white,
-                              child: Image.asset(
-                                  'assets/images/delivery-truck.png')),
-                        )
-                      ],
-                    ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 150,
+              child: Stack(children: [
+                Container(
+                  height: 130,
+                  decoration: BoxDecoration(
+                    color: Palette.scaffoldBg,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: const Offset(0, 1),
+                          color: Palette.primaryGreen.withOpacity(0.09),
+                          blurRadius: 2),
+                    ],
                   ),
-                  Positioned(
-                    top: 80,
-                    child: SizedBox(
-                        width: size.width,
-                        child: InputField(
-                          hint: 'What do you want to order',
-                          icon: const Icon(Icons.search),
-                        )),
-                  )
-                ]),
-              ),
-              Container(
-                width: size.width,
-                height: 50,
-                color: Colors.transparent,
-                child: ListView.builder(
-                  itemBuilder: (context, index) => GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _selectedindex = index;
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8, vertical: 4),
-                              decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                      offset: Offset(0, 2),
-                                      blurRadius: 2,
-                                      spreadRadius: 1,
-                                      color:
-                                          Palette.primaryGreen.withOpacity(0.1))
-                                ],
-                                borderRadius: BorderRadius.circular(20),
-                                color: _selectedindex == index
-                                    ? Palette.primaryGreen
-                                    : Palette.white,
-                              ),
-                              child: Row(
-                                children: [
-                                  CircleAvatar(
-                                      backgroundColor: _selectedindex == index
-                                          ? Palette.white
-                                          : Palette.scaffoldBg,
-                                      radius: 14,
-                                      child: Icon(
-                                        icons[index],
-                                        size: 16,
-                                        color: _selectedindex == index
-                                            ? Palette.borderGreen
-                                            : Palette.primaryGreen,
-                                      )),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(categories[index],
-                                      style: TextStyle(
-                                          color: _selectedindex == index
-                                              ? Palette.white
-                                              : Palette.borderGreen,
-                                          fontWeight: _selectedindex == index
-                                              ? FontWeight.bold
-                                              : FontWeight.w100,
-                                          fontSize: 18))
-                                ],
-                              ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Find Your',
+                            style: TextStyle(
+                                fontSize: 28, fontWeight: FontWeight.bold),
+                          ),
+                          Text('Favourite Food',
+                              style: TextStyle(
+                                fontSize: 24,
+                              )),
+                        ],
+                      ),
+                      CircleAvatar(
+                        radius: 26,
+                        child: CircleAvatar(
+                            radius: 24,
+                            foregroundColor: Colors.transparent,
+                            backgroundColor: Palette.white,
+                            child: Image.asset(
+                                'assets/images/delivery-truck.png')),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 80,
+                  child: SizedBox(
+                      width: size.width,
+                      child: InputField(
+                        hint: 'What do you want to order',
+                        icon: const Icon(Icons.search),
+                      )),
+                )
+              ]),
+            ),
+            Row(children: [
+              Text(
+                'Categories',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ]),
+            Container(
+              width: size.width,
+              height: 50,
+              color: Colors.transparent,
+              child: ListView.builder(
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedindex = index;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 4),
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: const Offset(0, 2),
+                                    blurRadius: 2,
+                                    spreadRadius: 1,
+                                    color:
+                                        Palette.primaryGreen.withOpacity(0.1))
+                              ],
+                              borderRadius: BorderRadius.circular(20),
+                              color: _selectedindex == index
+                                  ? Palette.primaryGreen
+                                  : Palette.white,
                             ),
-                          ],
-                        ),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                    backgroundColor: _selectedindex == index
+                                        ? Palette.white
+                                        : Palette.scaffoldBg,
+                                    radius: 14,
+                                    child: Icon(
+                                      icons[index],
+                                      size: 16,
+                                      color: _selectedindex == index
+                                          ? Palette.borderGreen
+                                          : Palette.primaryGreen,
+                                    )),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(categories[index],
+                                    style: TextStyle(
+                                        color: _selectedindex == index
+                                            ? Palette.white
+                                            : Palette.borderGreen,
+                                        fontWeight: _selectedindex == index
+                                            ? FontWeight.bold
+                                            : FontWeight.w100,
+                                        fontSize: 18))
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
                 ),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
               ),
-              const SizedBox(height: 15),
-              SizedBox(
-                height: size.height,
-                child: IndexedStack(
-                  index: _selectedindex,
-                  children: [
-                    Column(
-                      children: [
-                        CustomContainer(
-                            height: 150,
-                            width: 200,
-                            child: Column(
-                              children: [
-                                SizedBox(
-                                    height: 100,
-                                    child:
-                                        Image.asset('assets/images/Logo.png')),
-                                Container(
-                                  height: 50,
-                                )
-                              ],
-                            ))
-                      ],
-                    ),
-                    Container(),
-                    Container(),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+            const SizedBox(height: 15),
+            Expanded(
+              child: IndexedStack(
+                index: _selectedindex,
+                children: [
+                  ListView(
+                    children: [
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: fooditems.length,
+                          itemBuilder: (c, i) => Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: FoodCard(
+                              imagePath: fooditems[i].imageSrc,
+                              foodName: fooditems[i].name,
+                              price: '${fooditems[i].price}',
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(children: const [
+                        Text(
+                          'Today\'s Menu',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ]),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: fooditems.length,
+                          itemBuilder: (c, i) => Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: FoodCard(
+                                imagePath: fooditems[i].imageSrc,
+                                foodName: fooditems[i].name,
+                                price: '${fooditems[i].price}'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(),
+                  Container(),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
