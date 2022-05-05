a4c0aff73d2fc62bc9c649e1fc68622b03802e4c
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-05 18:53:26 +0100

    food item model init

---
+++ lib/home/models/food_item.dart
@@ -0,0 +1,13 @@
+class FoodItem {
+  String id;
+  int price;
+  String name;
+  String imageSrc;
+  String category;
+  FoodItem(
+      {required this.id,
+      required this.price,
+      required this.name,
+      required this.imageSrc,
+      required this.category});
+}
