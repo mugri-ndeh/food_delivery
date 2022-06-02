2e7d628ff65ac99538b98051f4855a8cceddb482
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-02 13:37:40 +0100

    profile changed

--- lib/home/screens/profile.dart
+++ lib/home/screens/profile.dart
@@ -1,5 +1,7 @@
-import 'package:delivery_app/util/palette.dart';
+import 'package:delivery_app/home/screens/orders/orders.dart';
+import 'package:delivery_app/util/helper.dart';
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 
 class Profile extends StatefulWidget {
@@ -10,167 +12,106 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  // late Authentication auth;
+  // late UserModel userModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // auth = Provider.of<Authentication>(context, listen: false);
+    // userModel = auth.loggedUser!;
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SingleChildScrollView(
+            child: Column(
           children: [
-            Stack(
-              // alignment: Alignment.center,
-              children: [
-                Positioned(
-                    child: Container(
-                  height: 300,
-                  decoration: BoxDecoration(color: Palette.scaffoldBg),
-                )),
-                Positioned(
-                    child: Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                      color: Palette.primaryGreen,
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(30),
-                          bottomRight: Radius.circular(30))),
-                )),
-                Positioned(
-                  left: size.width * 0.25,
-                  right: size.width * 0.25,
-                  top: 50,
-                  child: SizedBox(
-                    width: size.width * 0.5,
-                    child: const Center(
-                      child: Text(
-                        "Profile",
-                        style: TextStyle(fontSize: 34, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                    bottom: 0,
-                    left: size.width * 0.075,
-                    right: size.width * 0.075,
-                    child: Container(
-                      height: 200,
-                      width: size.width * 0.85,
-                      decoration: decoration,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CircleAvatar(
-                              radius: 50,
-                              backgroundColor: Palette.primaryGreen,
-                              child: const Icon(Icons.person),
-                            ),
-                            const Text(
-                              'user',
-                              style: TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
-                            ),
-                            const Text('')
-                          ]),
-                    )),
-              ],
-            ),
+            _profileHead(),
             const SizedBox(height: 20),
-            Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.edit)),
-                          title: Text("Edit"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.privacy_tip)),
-                          title: Text("Privacy Policy"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.help)),
-                          title: Text("About"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(
-                            child: Icon(
-                              Icons.logout_rounded,
-                              size: 16,
-                              color: Colors.white,
-                            ),
-                            backgroundColor: Colors.red,
-                          ),
-                          title: Text(
-                            "Logout",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                  ],
-                ))
+            _profileCard(
+                title: 'My orders',
+                subtitle: 'Already have 12 orders',
+                destination: OrdersPage()),
+            const SizedBox(height: 10),
+            _profileCard(title: 'Edit profile', subtitle: 'Edit user details'),
+            const SizedBox(height: 10),
+            _profileCard(title: 'My reviews', subtitle: 'Reviews for 4 items'),
+            const SizedBox(height: 10),
+            _profileCard(title: 'Settings', subtitle: 'Change theme'),
           ],
-        ),
+        )),
       ),
     );
   }
 
-  var decoration = BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(10),
-    boxShadow: [
-      BoxShadow(
-        offset: Offset(1, 2),
-        blurStyle: BlurStyle.outer,
-        blurRadius: 3,
-        spreadRadius: 1,
-        color: Color.fromARGB(255, 228, 222, 222).withOpacity(0.2),
+  Widget _profileHead() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'My profile',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(radius: 30),
+            const SizedBox(width: 18),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Maestro',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text(
+                  '',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _profileCard(
+      {required String title, required String subtitle, Widget? destination}) {
+    return GestureDetector(
+      onTap: () {
+        push(context, destination!);
+      },
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(1, 2),
+              blurStyle: BlurStyle.outer,
+              blurRadius: 3,
+              spreadRadius: 1,
+              color: Color.fromARGB(255, 228, 222, 222).withOpacity(0.2),
+            ),
+          ],
+        ),
+        child: ListTile(
+          selectedTileColor: Colors.white,
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          trailing: Icon(Icons.arrow_forward_ios),
+          subtitle: Text(subtitle),
+        ),
       ),
-    ],
-  );
+    );
+  }
 }
