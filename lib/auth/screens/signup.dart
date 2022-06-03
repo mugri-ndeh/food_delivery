9080333a63909c768476fb2b90801868f759644d
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 07:23:07 +0100

    phone added

--- lib/auth/screens/signup.dart
+++ lib/auth/screens/signup.dart
@@ -2,9 +2,13 @@ import 'package:delivery_app/auth/screens/login.dart';
 import 'package:delivery_app/auth/widgets/custom_Button.dart';
 import 'package:delivery_app/auth/widgets/custom_fields.dart';
 import 'package:delivery_app/home/screens/base.dart';
+import 'package:delivery_app/models/customer.dart';
+import 'package:delivery_app/util/helper.dart';
 import 'package:delivery_app/util/palette.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/country_picker_dialog.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -15,10 +19,16 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   final _userNameController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  String initialCountry = 'CM';
+  String? locale;
+  final TextEditingController _controller = TextEditingController();
+  String number = '';
 
   @override
   void initState() {
@@ -51,8 +61,25 @@ class _SignUpState extends State<SignUp> {
                         child: Image.asset('assets/images/deliver.png')),
                   ),
                   InputField(
+                    validator: validateName,
                     controller: _userNameController,
                     hint: 'username',
+                    icon: const Icon(Icons.person),
+                  ),
+                  const SizedBox(height: 8),
+
+                  InputField(
+                    validator: validateName,
+                    controller: _firstNameController,
+                    hint: 'firstname',
+                    icon: const Icon(Icons.person),
+                  ),
+                  const SizedBox(height: 8),
+
+                  InputField(
+                    validator: validateName,
+                    controller: _lastNameController,
+                    hint: 'lastname',
                     icon: const Icon(Icons.person),
                   ),
                   const SizedBox(height: 8),
@@ -63,6 +90,47 @@ class _SignUpState extends State<SignUp> {
                     icon: const Icon(Icons.mail),
                   ),
                   const SizedBox(height: 8),
+                  IntlPhoneField(
+                    dropdownIcon:
+                        Icon(Icons.arrow_drop_down, color: Palette.buttonColor),
+                    controller: _controller,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    dropdownIconPosition: IconPosition.leading,
+                    initialCountryCode: initialCountry,
+                    pickerDialogStyle: PickerDialogStyle(),
+                    decoration: InputDecoration(
+                      // hintText: 'Phone Number',
+                      labelText: 'Phone Number',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          borderSide: BorderSide(
+                              color: Palette.primaryColor.withOpacity(0.3),
+                              width: 2.0)),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.red),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Palette.buttonColor),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Palette.primaryColor.withOpacity(0.1)),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    onChanged: (phone) {
+                      print(phone.completeNumber);
+                      setState(() {
+                        number = phone.completeNumber;
+                      });
+                    },
+                    onCountryChanged: (country) {
+                      print('Country changed to: ' + country.name);
+                    },
+                  ),
+                  const SizedBox(height: 10),
                   InputField(
                     controller: _passwordController,
                     hint: 'password',
@@ -89,6 +157,8 @@ class _SignUpState extends State<SignUp> {
                   Center(
                       child: AuthButton(
                     onTap: () {
+                      Customer user = Customer();
+
                       // push(context, Completeprofile());
                       Navigator.push(
                           context,
@@ -100,12 +170,7 @@ class _SignUpState extends State<SignUp> {
                   )),
                   // const SizedBox(height: 50),
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                              builder: (context) => LoginScreen()));
-                    },
+                    onTap: () {},
                     child: Center(
                       child: SizedBox(
                         height: 50,
