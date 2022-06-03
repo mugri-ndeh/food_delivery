e7203a3082f11b8099aa03ae154e53212dedc77b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 07:22:49 +0100

    loke

---
+++ lib/home/screens/favourites/favourites_provider.dart
@@ -0,0 +1,79 @@
+import 'package:delivery_app/home/models/food_item.dart';
+import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
+
+class FavouritesHelper with ChangeNotifier {
+  final LocalStorage storage = LocalStorage('favourites');
+  List favourites = [];
+  bool isFavourite = false;
+
+  init() async {
+    favourites = await getFavourites();
+    print('INIT FAV HELPER');
+    notifyListeners();
+  }
+
+  FavouritesHelper() {
+    init();
+  }
+
+  bool isfavourite(FoodItem song) {
+    bool favourite = false;
+    int i;
+    for (i = 0; i < favourites.length; i++) {
+      if (song.id == FoodItem.fromJson(favourites[i]).id) {
+        favourite = true;
+      }
+    }
+    isFavourite = favourite;
+    notifyListeners();
+    print(favourite);
+    return favourite;
+  }
+
+  addFavourites(FoodItem foodItem) {
+    favourites.add(foodItem.toJson());
+    _saveToStorage();
+    notifyListeners();
+  }
+
+  remove(FoodItem food) async {
+    await storage.ready;
+
+    favourites.removeWhere((element) {
+      //print(element.values);
+      var val = false;
+      print(food.toJson()['id']);
+
+      if (element['id'] == food.toJson()['id']) {
+        val = true;
+      } else {
+        val = false;
+      }
+      return val;
+
+      //return element.values == song.toJson().values;
+    });
+    print(favourites);
+    _saveToStorage();
+    notifyListeners();
+    //storage.deleteItem('favourite $name');
+  }
+
+  _saveToStorage() async {
+    await storage.ready;
+    // List existing = await getFavourites();
+    // existing.add(song.toJson());
+    storage.setItem('favourite', favourites);
+    print('SAVED CORRECTLY');
+    notifyListeners();
+  }
+
+  getFavourites() async {
+    await storage.ready;
+    List fav = await storage.getItem('favourite') ?? [];
+    notifyListeners();
+
+    return fav;
+  }
+}
