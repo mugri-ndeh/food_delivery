31ce61ae80e1592eb7e85205b341af411de4d098
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 07:22:39 +0100

    lol

---
+++ lib/home/screens/cart/checkout.dart
@@ -0,0 +1,56 @@
+import 'package:delivery_app/api/api.dart';
+import 'package:delivery_app/auth/widgets/custom_Button.dart';
+import 'package:delivery_app/util/palette.dart';
+import 'package:flutter/material.dart';
+
+class CheckoutPage extends StatefulWidget {
+  CheckoutPage({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<CheckoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Palette.scaffoldBg,
+        title: const Text(
+          'Checkout',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Palette.white,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    height: 110,
+                    width: size.width,
+                    child: const Center(
+                        child:
+                            Text('Your order is on it\'s way hang in there'))),
+              ),
+            ),
+            AuthButton(
+                text: 'Go back',
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
