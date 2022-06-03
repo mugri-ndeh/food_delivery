6308463c729a2cea1f37d880e60ff6cc8d55ce59
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 07:20:52 +0100

    cart model

--- lib/home/models/cart.dart
+++ lib/home/models/cart.dart
@@ -1,1 +1,44 @@
+import 'package:delivery_app/home/models/food_item.dart';
 
+class Cart {
+  int id;
+  List<CartItem> items;
+
+  Cart({
+    this.id = 0,
+    this.items = const [],
+  });
+
+  factory Cart.fromJson(Map<String, dynamic> json) {
+    return Cart(
+      id: json['id'],
+      items: json['items'],
+    );
+  }
+
+  toJson() {
+    return {
+      'id': id,
+      'items': items,
+    };
+  }
+}
+
+class CartItem {
+  FoodItem? item;
+  int qty;
+  CartItem({this.item, this.qty = 0});
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    return CartItem(
+      qty: json['qty'],
+      item: json['item'],
+    );
+  }
+
+  tojson() {
+    return {
+      'item': item,
+      'qty': qty,
+    };
+  }
+}
