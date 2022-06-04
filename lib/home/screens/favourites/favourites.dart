25560af4dd6b8c239b58ab7305ad9961ace2e7c8
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-04 17:14:19 +0100

    showing dynamically

--- lib/home/screens/favourites/favourites.dart
+++ lib/home/screens/favourites/favourites.dart
@@ -1,6 +1,9 @@
 import 'package:delivery_app/api/api.dart';
 import 'package:delivery_app/auth/widgets/custom_fields.dart';
+import 'package:delivery_app/home/models/food_item.dart';
+import 'package:delivery_app/home/screens/favourites/favourites_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Favourites extends StatefulWidget {
   Favourites({Key? key}) : super(key: key);
@@ -13,14 +16,18 @@ class _FavouritesState extends State<Favourites> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(
-          children: [FavouriteCard(foodItem: Api().fooditems[3])],
+        body: SafeArea(child: Consumer<FavouritesHelper>(builder: (_, fav, __) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: ListView.builder(
+          itemCount: fav.favourites.length,
+          itemBuilder: ((context, index) => FavouriteCard(
+              foodItem: FoodItem.fromJson(fav.favourites[index]))),
         ),
-      ),
-    )));
+        // child: Column(
+        //   children: [FavouriteCard(foodItem: Api().fooditems[3])],
+        // ),
+      );
+    })));
   }
 }
