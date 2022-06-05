1e6dcf5780415ccd2c922a561621689de4532b15
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-05 09:20:16 +0100

    add and remove to cart works

--- lib/home/screens/cart/cart.dart
+++ lib/home/screens/cart/cart.dart
@@ -1,9 +1,13 @@
 import 'package:delivery_app/api/api.dart';
 import 'package:delivery_app/auth/widgets/custom_Button.dart';
+import 'package:delivery_app/home/models/cart.dart';
+import 'package:delivery_app/home/models/food_item.dart';
+import 'package:delivery_app/home/screens/cart/cart_provider.dart';
 import 'package:delivery_app/home/screens/cart/checkout.dart';
 import 'package:delivery_app/util/helper.dart';
 import 'package:delivery_app/util/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -13,6 +17,27 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  int total = 0;
+  getTotal(CartHelper cart) {
+    total = 0;
+    for (int i = 0; i < cart.cartItems.length; i++) {
+      CartItem item = CartItem.fromJson(cart.cartItems[i]);
+      FoodItem foodItem = FoodItem.fromJson(item.item!);
+      total = total + int.parse(foodItem.price.replaceAll('XAF', '').trim());
+    }
+    print(total);
+  }
+
+  late CartHelper cart;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cart = Provider.of<CartHelper>(context, listen: false);
+    getTotal(cart);
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -37,108 +62,132 @@ class _CartPageState extends State<CartPage> {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: Api().fooditems.length ~/ 3,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(top: 8),
-                      decoration: BoxDecoration(
-                          color: Palette.white,
-                          borderRadius: BorderRadius.circular(24)),
-                      height: 100,
-                      width: size.width,
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            height: 100,
-                            width: 100,
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(24),
-                                child: Image.asset(
-                                  Api().fooditems[index].image,
-                                  fit: BoxFit.cover,
-                                )),
-                          ),
-                          const SizedBox(width: 8),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 8),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    Api().fooditems[index].name,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
-                                  Text(
-                                    Api().fooditems[index].description,
-                                    style: const TextStyle(color: Colors.grey),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  Text(
-                                    '${Api().fooditems[index].price} XAF',
-                                    style:
-                                        TextStyle(color: Palette.primaryColor),
-                                  ),
-                                ],
-                              ),
+        child: Consumer<CartHelper>(builder: (_, cart, __) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: cart.cartItems.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      CartItem item = CartItem.fromJson(cart.cartItems[index]);
+                      FoodItem foodItem = FoodItem.fromJson(item.item!);
+                      print(item.item);
+                      return Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        decoration: BoxDecoration(
+                            color: Palette.white,
+                            borderRadius: BorderRadius.circular(24)),
+                        height: 100,
+                        width: size.width,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(24),
+                                  child: Image.asset(
+                                    'assets/images/' + foodItem.image,
+                                    fit: BoxFit.cover,
+                                  )),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                            child: SizedBox(
-                                width: 90,
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
                                 child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
                                   children: [
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.close),
-                                      color: Palette.primaryColor,
+                                    Text(
+                                      foodItem.name,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
                                     ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        CistomCircularButton(
-                                            outlined: true,
-                                            onTap: () {},
-                                            icon: Icons.remove),
-                                        const Text(
-                                          '02',
-                                        ),
-                                        CistomCircularButton(
-                                            outlined: false,
-                                            onTap: () {},
-                                            icon: Icons.add),
-                                      ],
-                                    )
+                                    Text(
+                                      foodItem.description,
+                                      style:
+                                          const TextStyle(color: Colors.grey),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      foodItem.price,
+                                      style: TextStyle(
+                                          color: Palette.primaryColor),
+                                    ),
                                   ],
-                                )),
-                          )
-                        ],
-                      ),
-                    );
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: SizedBox(
+                                  width: 100,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {
+                                          cart.remove(index);
+                                          setState(() {
+                                            getTotal(cart);
+                                          });
+                                        },
+                                        icon: const Icon(Icons.close),
+                                        color: Palette.primaryColor,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const Text(
+                                            'Quantity: ',
+                                            style: TextStyle(fontSize: 16),
+                                          ),
+                                          Text(
+                                            item.qty.toString(),
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16),
+                                          ),
+                                          const SizedBox(width: 10),
+                                        ],
+                                      )
+                                    ],
+                                  )),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Text(total.toString()),
+              const SizedBox(height: 10),
+              AuthButton(
+                  text: 'Checkout',
+                  onTap: () {
+                    // push(context, const CheckoutPage());
+
+                    // print(cart.cartItems);
+                    int total = 0;
+                    for (int i = 0; i < cart.cartItems.length; i++) {
+                      CartItem item = CartItem.fromJson(cart.cartItems[i]);
+                      FoodItem foodItem = FoodItem.fromJson(item.item!);
+                      total = total +
+                          int.parse(
+                              foodItem.price.replaceAll('XAF', '').trim());
+                    }
+                    print(total);
                   }),
-            ),
-            const SizedBox(height: 10),
-            AuthButton(
-                text: 'Checkout',
-                onTap: () {
-                  push(context, const CheckoutPage());
-                }),
-            const SizedBox(height: 10),
-          ],
-        ),
+              const SizedBox(height: 10),
+            ],
+          );
+        }),
       )),
     );
   }
