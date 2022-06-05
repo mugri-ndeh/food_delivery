127614f62606f36f8158266a8d23fa9b46c899f5
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-05 09:22:08 +0100

    convert to map

--- lib/home/models/orders.dart
+++ lib/home/models/orders.dart
@@ -1,16 +1,39 @@
-import 'package:delivery_app/home/models/food_item.dart';
+import 'package:delivery_app/home/models/cart.dart';
 
 class Order {
   String orderId;
-  List<FoodItem> foodItems;
+  CartItem? foodItems;
   int quantity;
   String state;
   String userId;
+  String priceTotal;
 
   Order(
       {this.orderId = '',
-      this.foodItems = const [],
+      this.foodItems,
       this.quantity = 0,
       this.state = '',
+      this.priceTotal = '0',
       this.userId = ''});
+  factory Order.fromJson(Map<String, dynamic> json) {
+    return Order(
+      orderId: json['id'],
+      foodItems: json['food_items'],
+      quantity: json['qty'],
+      state: json['o_state'],
+      userId: json['user_id'],
+      priceTotal: json['price_total'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': orderId,
+      'food_items': foodItems,
+      'qty': quantity,
+      'o_state': state,
+      'user_id': userId,
+      'price_total': priceTotal,
+    };
+  }
 }
