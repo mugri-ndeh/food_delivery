2b473ff43d136dbcb975966ca72f1569d3088f2b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-06 03:44:52 +0100

    edit profile ok

--- lib/home/screens/profile/edit_profile.dart
+++ lib/home/screens/profile/edit_profile.dart
@@ -1,9 +1,14 @@
+import 'package:delivery_app/api/test_api.dart';
+import 'package:delivery_app/auth/provider/auth.dart';
 import 'package:delivery_app/auth/widgets/custom_Button.dart';
 import 'package:delivery_app/auth/widgets/custom_fields.dart';
+import 'package:delivery_app/models/customer.dart';
+import 'package:delivery_app/util/helper.dart';
 import 'package:delivery_app/util/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/country_picker_dialog.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:provider/provider.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
@@ -13,18 +18,28 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   String initialCountry = 'CM';
   String? locale;
   final TextEditingController _controller = TextEditingController();
   String number = '';
-
+  late Authentication auth;
+  late Customer user;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    auth = Provider.of<Authentication>(context, listen: false);
+    user = auth.loggedUser!;
+    _usernameController.text = user.username;
+    _firstNameController.text = user.firstname;
+    _lastNameController.text = user.lastname;
+    _emailController.text = user.email;
+    _controller.text = user.phonenumber.replaceAll('+237', '').trim();
   }
 
   @override
@@ -68,14 +83,30 @@ class _EditProfileState extends State<EditProfile> {
                 ),
                 const SizedBox(height: 10),
                 InputField(
-                  controller: _emailController,
-                  hint: 'email',
-                  icon: const Icon(Icons.mail),
-                ),
-                const SizedBox(height: 10),
-                InputField(
+                  validator: validateName,
                   controller: _usernameController,
                   hint: 'username',
+                  icon: const Icon(Icons.person),
+                ),
+                const SizedBox(height: 8),
+                InputField(
+                  validator: validateName,
+                  controller: _firstNameController,
+                  hint: 'firstname',
+                  icon: const Icon(Icons.person),
+                ),
+                const SizedBox(height: 8),
+                InputField(
+                  validator: validateName,
+                  controller: _lastNameController,
+                  hint: 'lastname',
+                  icon: const Icon(Icons.person),
+                ),
+                const SizedBox(height: 8),
+                InputField(
+                  validator: validateEmail,
+                  controller: _emailController,
+                  hint: 'email',
                   icon: const Icon(Icons.mail),
                 ),
                 const SizedBox(height: 20),
@@ -124,7 +155,21 @@ class _EditProfileState extends State<EditProfile> {
                   child: AuthButton(
                     text: 'Save data',
                     onTap: () {
-                      if (_formKey.currentState!.validate()) {}
+                      if (_formKey.currentState!.validate()) {
+                        // Customer user = auth.loggedUser!;
+                        user.username = _usernameController.text;
+                        user.firstname = _firstNameController.text;
+                        user.lastname = _lastNameController.text;
+                        user.email = _emailController.text;
+                        user.phonenumber = number;
+                        showProgress(context, 'Saving info...', true);
+                        TestApi.editProfile(user).then((value) {
+                          auth.returnUser(user.id);
+                          hideProgress();
+                          showAlertDialog(context, 'Success',
+                              'Details updated successfully');
+                        });
+                      }
                     },
                   ),
                 )
