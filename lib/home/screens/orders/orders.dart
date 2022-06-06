909293cb23ca498ff2aaaadd97508b291c16faf7
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-06 08:17:19 +0100

    show no orders if empty screen

--- lib/home/screens/orders/orders.dart
+++ lib/home/screens/orders/orders.dart
@@ -1,15 +1,18 @@
 import 'package:delivery_app/api/api.dart';
+import 'package:delivery_app/home/models/orders.dart';
 import 'package:delivery_app/util/palette.dart';
 import 'package:flutter/material.dart';
 
 class OrdersPage extends StatefulWidget {
   const OrdersPage({Key? key}) : super(key: key);
+  // final List<Order> orders;
 
   @override
   State<OrdersPage> createState() => _OrdersPageState();
 }
 
 class _OrdersPageState extends State<OrdersPage> {
+  List<Order> orders = [];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -25,59 +28,63 @@ class _OrdersPageState extends State<OrdersPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: SingleChildScrollView(
-            child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Palette.white,
-                borderRadius: BorderRadius.circular(24),
-              ),
-              height: 110,
-              width: size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(24),
-                            child: Image.asset(
-                              Api().fooditems[4].image,
-                              fit: BoxFit.cover,
-                            )),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
+        child: orders.isEmpty
+            ? Center(
+                child: Text('You have no Orders'),
+              )
+            : ListView.builder(
+                itemCount: orders.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Palette.white,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    height: 110,
+                    width: size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
                           children: [
-                            const Text('Title'),
-                            const Text('caption'),
-                            const Text('detail'),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text('item count'),
-                                Text('Price'),
-                              ],
+                            SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(24),
+                                  child: Image.asset(
+                                    Api().fooditems[4].image,
+                                    fit: BoxFit.cover,
+                                  )),
                             ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(orders[0].foodItems![0]),
+                                  const Text('caption'),
+                                  const Text('detail'),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: const [
+                                      Text('item count'),
+                                      Text('Price'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 10),
                           ],
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                    ],
-                  ),
-                  Row(),
-                ],
-              ),
-            )
-          ],
-        )),
+                        Row(),
+                      ],
+                    ),
+                  );
+                }),
       ),
     );
   }
