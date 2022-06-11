4dc68e976703b5296f8990d42d154e8cb1ff608b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 08:47:24 +0100

    get orders functions ok

--- lib/api/test_api.dart
+++ lib/api/test_api.dart
@@ -40,7 +40,7 @@ class TestApi {
     final response = await http.post(url, body: customer.toJson());
 
     // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
+    print('Response body: ${response.body}');
 
     Map<String, dynamic> result = json.decode(response.body);
     print(result);
@@ -77,7 +77,7 @@ class TestApi {
     order.encodeList();
     var url = Uri.parse('${Env.URL_PREFIX}/customer/create_order.php');
     final response = await http.post(url, body: {
-      'food_items': json.encode(order.foodItems),
+      'food_items': jsonEncode(order.foodItems),
       'qty': order.quantity.toString(),
       'o_state': order.state,
       'price_total': order.priceTotal,
@@ -94,58 +94,46 @@ class TestApi {
     // return foods;
   }
 
-  static Future getOrders(int id) async {
+  static Future<List<Order>> getOrders(int id) async {
     var url = Uri.parse('${Env.URL_PREFIX}/customer/get_orders.php');
     final response = await http.post(url, body: {
       'id': id.toString(),
     });
     // print('Response status: ${response.statusCode}');
     // print('Response body: ${response.body}');
-    // int orderId;
-    // List? foodItems;
-    // int quantity;
-    // String state;
-    // int userId;
-    // String priceTotal;
 
     Map<String, dynamic> result = json.decode(response.body);
-    List foodItems = (result['state']);
-    List testItems = [];
+    print(result['state'][0]['items']);
 
-    // print(foodItems[0]);
+    List orders = result['state'];
 
-    // for (var item in foodItems) {
-    //   testItems.add(Order.fromJson(item));
+    print(orders.length);
 
-    //   var foods = jsonDecode(item['food_items']);
-    //   // print(item['id']);
-    //   print('\n');
-    //   // for (var food in foods) {
-    //   //   print(food);
-    //   //   print('\n');
-    //   // }
-    // }
+    List<Order> ordersReturened = [];
 
-    List orderItems =
-        result['state'].map((json) => Order.fromJson((json))).toList();
-    // print(jsonDecode(result['state'][0]['food_items'])[0]['item']);
-    // jsonDecode(result['state'][0]['food_items']);
-    // print(orderItems[0].foodItems[]);
-    // print();
-    // var lol = jsonDecode(result['state'][0]['food_items']);
-    // var hey = json.decode(lol);
-    // var hehe = json.decode('"' + hey + '"');
+    for (var order in orders) {
+      // List<FoodItem> foodItems =
+      //     order["items"].map((e) => FoodItem.fromJson(e));
 
-    // print(jsonEncode(result['state'][0]['food_items'].replaceAll("\\", "")));
-    // List orders = result['state'].map((json) => Order.fromJson(json)).toList();
+      // print(order["items"]);
+      List<FoodItem> ordersFromFood = [];
+      Order ordersOrder = Order();
+      ordersFromFood = order["items"]
+          .map((e) => FoodItem.fromJson(e))
+          .toList()
+          .cast<FoodItem>();
 
-    //  List orders = Order.fromJson(result['state']);
-    // return orders;
+      ordersOrder = Order.fromBbJson(order["order"]);
+      ordersOrder.foodItems = ordersFromFood.map((e) => e.toJson()).toList();
+      ordersReturened.add(ordersOrder);
+    }
+
+    return ordersReturened;
   }
 }
 
 class Env {
-  // static String URL_PREFIX = "http://10.0.2.2/food_app/functions";
-  static String URL_PREFIX =
-      "https://maestrofood.000webhostapp.com/food_app/functions";
+  static String URL_PREFIX = "http://10.0.2.2/food_app_php/functions";
+  // static String URL_PREFIX =
+  //     "https://maestrofood.000webhostapp.com/food_app/functions";
 }
